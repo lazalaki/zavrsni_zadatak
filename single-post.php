17d9bd3d07896849e64fbffbd64035c898e026bf
Lazar Erbes <lakierbes@gmail.com>
2019-04-16 20:31:46 +0200

    Zadatak 6.13

--- single-post.php
+++ single-post.php
@@ -2,11 +2,9 @@
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
-    include("database.php"); 
+    include ('database.php'); 
 ?>
-
-
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 <head>
     <meta charset="utf-8">
@@ -14,35 +12,35 @@
     <meta name="description" content="">
     <meta name="author" content="">
     <link rel="icon" href="../../../../favicon.ico">
-     <title>Vivify Blog</title>
+    <title>Vivify Blog</title>
+    
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
     <link href="styles/blog.css" rel="stylesheet">
-    <link href="styles/style.css" rel="stylesheet">
+    <link href="styles/styles.css" rel="stylesheet">
 </head>
 <?php
     $sql = "SELECT posts.id, posts.title, posts.created_at, posts.author, posts.body
     FROM posts WHERE posts.id = {$_GET['post_id']} ";
-    
     $statement = $connection->prepare($sql);
     $statement->execute();
     $statement->setFetchMode(PDO::FETCH_ASSOC);
     $singlePost = $statement->fetchAll()[0];
 ?>
 <body>
-    <?php include 'header.php' ?>
+<?php include 'header.php' ?>
 <main role="main" class="container">
     <div class="row">
         <div class="col-sm-8 blog-main">
             <div class="blog-post">
                 <a href= "single-post.php?post_id=<?php echo($singlePost['id']) ?>"><h2 class="blog-post-title"><?php echo ($singlePost['title']); ?></h2></a>
-                
                 <p class="blog-post-meta"><?php echo ($singlePost['created_at']); ?> by <a href="#"><?php echo ($singlePost['author']); ?></a></p>
                 <p><?php echo ($singlePost['body']); ?></p>
             </div>
+            <?php include 'comments.php' ?>
         </div>
-    <?php include 'sidebar.php' ?>
+        <?php include 'sidebar.php' ?>
     </div>
 </main>
-    <?php include 'footer.php' ?>
+<?php include 'footer.php' ?>
 </body>
 </html>
